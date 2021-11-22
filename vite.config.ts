84e082ef5e3a4a9a4cd775a4ef320c3d01378ec9
Micheal Parks <michealparks1989@gmail.com>
2021-11-22 00:25:24 -0500

    add glsl dep

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import vitePluginString from 'vite-plugin-string'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  // publicDir: 'static',
+  plugins: [react(), vitePluginString()],
 })
